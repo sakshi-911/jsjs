2e31466305e1fae5e3818f914023fefd4fd83dc2
sakshi-911 <astrosakshisingh@gmail.com>
2024-12-17 14:00:56 +0000

    done

---
+++ basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let accountEmail="sakshi@gmail.com"
+var accountPassword= "1234"
+accountCity="jaipur"
+let accountState;
+
+//accountId=2 // not allowed
+accountEmail="dd@kdj"
+accountPassword="32873"
+accountCity="blr"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3) ;// code readability sholud be high
+console.log("sakshi")
+
+let name="aurora"
+let age=21
+let isLoggedIn=false
+let state;
+
+// number=> 2^53
+// bigint 
+// string=>" "
+// boolean => true / false
+// null=> standalone value
+// undefined => when variable is declared but not assigned 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
